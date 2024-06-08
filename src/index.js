39b55b9f893998ce8ee12af42b647d1aad09a98c
Sergei Ivankov <sergeiivankov@pm.me>
2024-06-08 19:11:48 +0300

    Separate external libraries, remove Google Fonts

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import 'bulma/css/bulma.min.css';
 import './style.css';
-import 'docx-preview';
 
 import App from './components/App.svelte';
 
--- src/rollup.config.js
+++ src/rollup.config.js
@@ -1,10 +1,11 @@
-import copy from 'rollup-plugin-copy';
-import css from 'rollup-plugin-import-css';
+import commonjs from '@rollup/plugin-commonjs';
 import json from '@rollup/plugin-json';
 import resolve from '@rollup/plugin-node-resolve';
+import terser from '@rollup/plugin-terser';
+import copy from 'rollup-plugin-copy';
+import css from 'rollup-plugin-import-css';
 import serve from 'rollup-plugin-serve';
 import svelte from 'rollup-plugin-svelte';
-import terser from '@rollup/plugin-terser';
 
 const IS_DEV = !!process.env.ROLLUP_WATCH;
 
@@ -19,11 +20,16 @@ const plugins = [
       handler(warning);
     }
   }),
+  commonjs(),
   resolve({ browser: true }),
   css({ output: 'app.css' }),
   json(),
   copy({
-    targets: [{ src: 'src/assets/*', dest: 'dist' }]
+    targets: [
+      { src: 'src/assets/*', dest: 'dist' },
+      { src: 'node_modules/jszip/dist/jszip.min.js', dest: 'dist/js' },
+      { src: 'node_modules/docx-preview/dist/docx-preview.min.js', dest: 'dist/js' }
+    ]
   })
 ];
 
@@ -36,7 +42,7 @@ if(IS_DEV) {
 export default {
   input: 'src/index.js',
   output: {
-    file: 'dist/app.js',
+    file: 'dist/js/app.min.js',
     format: 'iife'
   },
   plugins,
