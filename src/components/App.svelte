4ed008c457914f79715318f5f2680d079697cca7
Sergei Ivankov <sergeiivankov@pm.me>
2024-09-05 17:54:15 +0300

    Export/import using local file

--- src/components/App.svelte
+++ src/components/App.svelte
@@ -1,8 +1,12 @@
 <script>
   import { version } from '../../package.json';
 
+  import { exportDB, importInto } from 'dexie-export-import';
+
   import { fade, fly } from 'svelte/transition';
-  import { alertData } from '../common/ui.js';
+  import db from '../common/db.js';
+  import { alertData, showAlert } from '../common/ui.js';
+  import { readFile, displayError } from '../common/utils.js';
 
   import Documents from './Documents/Layout.svelte';
   import Templates from './Templates.svelte';
@@ -15,7 +19,64 @@
   const setSection = section => {
     activeSection = section;
   };
+
+  const importFromFile = () => {
+    const input = document.createElement('input');
+    input.type = 'file';
+    input.accept = '.json';
+
+    input.onchange = async e => {
+      const data = await readFile(e.target.files[0]);
+
+      try {
+        await importInto(db, new Blob([data]), { overwriteValues: true });
+      } catch(err) {
+        return displayError('Ошибка импорта базы данных', err);
+      }
+
+      showAlert('База данных успешно испортирована');
+    };
+
+    input.click();
+  };
+
+  const exportToFile = async () => {
+    let blob;
+
+    try {
+      blob = await exportDB(db);
+    } catch(err) {
+      return displayError('Ошибка чтения базы данных', err);
+    }
+
+    let handle;
+
+    try {
+      handle = await showSaveFilePicker({
+        id: 'naidoc',
+        startIn: 'desktop',
+        suggestedName: 'naidoc.json',
+        types: [{
+          accept: { 'text/json': ['.json'] },
+        }]
+      });
+    } catch {
+      return;
+    }
+
+    try {
+      const writableStream = await handle.createWritable();
+      await writableStream.write(blob);
+      await writableStream.close();
+    } catch(err) {
+      return displayError('Ошибка записи файла', err);
+    }
+
+    showAlert('База данных успешно сохранена');
+  };
 </script>
+
+<svelte:window on:beforeunload|preventDefault/>
 
 <div class="navbar is-light">
   <div class="container">
@@ -39,6 +100,8 @@
         <div class="navbar-item has-dropdown is-hoverable">
           <a class="navbar-link">Меню</a>
           <div class="navbar-dropdown is-right">
+            <a class="navbar-item" on:click|preventDefault={ exportToFile }>Экспорт в файл</a>
+            <a class="navbar-item" on:click|preventDefault={ importFromFile }>Импорт из файла</a>
             <a href="https://github.com/naidoc/naidoc/blob/main/docs/ru/changelog.md" class="navbar-item" target="_blank">
               История изменений
             </a>
